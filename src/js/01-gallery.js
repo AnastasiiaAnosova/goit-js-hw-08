31eb545ea4fa1a39730fcafe9c86964de57f50d3
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-08 21:22:14 +0300

    task 1 finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+SimpleLightbox;
+
+const galleryElement = document.querySelector('.gallery');
+const galleryItemsElement = galleryItems.map((image) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+    <img 
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+    />
+    </a>
+    </li>`;
+}).join('');
+
+galleryElement.innerHTML = galleryItemsElement;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
