6f4c0237b2a95d11a6500894e3d32ef9985060ea
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-08 22:23:20 +0300

    install "@vimeo/player"

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
 
+import Player from '@vimeo/player';
+
+const inframe = document.querySelector('inframe');
+const player = new Vimeo.Player(inframe);
+
+const onPlay = function (data) {
+    console.log(data.timeupdate);
+};
+
+player.on('play', onPlay);
+
+
+
+player.getVideoTitle().then(onTitle);
+
+function onTitle(title) {
+    console.log('title:', title);
+}
