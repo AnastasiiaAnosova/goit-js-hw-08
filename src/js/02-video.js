348e3667cbdbf45f4d995921f298390eaaa9acc0
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-09 15:23:10 +0300

    finished task 2
    I used the Vimeo player library.
    I added the library as a project dependency via npm.
    Used the on() method to track the timeupdate event - updating the playback time.
    Save the playback time to the local storage. The key for the storage is the string "videoplayer-current-time".
    When the page reloads, I use the setCurrentTime() method to resume playback from the saved position.
    I added the lodash.throttle library to the project so that the playback time is updated in the storage no more than once per second.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,33 @@
-
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const PLAY_TIME_KEY = "videoplayer-current-time";
+let playTime = 0;
+let serializedState;
 
-const inframe = document.querySelector('inframe');
-const player = new Vimeo.Player(inframe);
+player.on('timeupdate', throttle(onTimeUpDate, 1000));
 
-const onPlay = function (data) {
-    console.log(data.timeupdate);
-};
+try {
+    serializedState = localStorage.getItem(PLAY_TIME_KEY);
+    if (serializedState !== null) {
+        playTime = JSON.parse(serializedState);
+        player.setCurrentTime(playTime).catch(function (error) {
+            console.error("Set current time error:", error.message);
+        })
+    }
 
-player.on('play', onPlay);
+} catch (error) {
+    console.error("Set state error: ", error.message);
+}
 
-
-
-player.getVideoTitle().then(onTitle);
-
-function onTitle(title) {
-    console.log('title:', title);
+function onTimeUpDate(data) {
+    playTime = data.seconds;
+    // console.log(playTime);
+    try {
+        serializedState = JSON.stringify(playTime);
+        localStorage.setItem(PLAY_TIME_KEY, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
 }
