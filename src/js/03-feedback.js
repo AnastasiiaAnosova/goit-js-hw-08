ea1bb61db3f0e531989337c037fbbe89631ce1f3
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-09 17:35:50 +0300

    finished task 3
    I monitor the input event on the form, and each time I write an object with the email and message fields to the local storage, which stores the current values of the form fields. The key for the storage is the string "feedback-form-state".
    When the page loads, I check the state of the storage, and if there is any saved data there, I fill in the form fields with it. Otherwise, the fields will be empty.
    During the form summit, I clear the storage and form fields, and display an object with the email, message fields and their current values in the console.
    I use the lodash.throttle library to ensure that the storage is updated no more than once every 500 milliseconds.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const DATE_FORM = "feedback-form-state";
+let dataInputs = {};
+const formFeedback = document.querySelector('.feedback-form');
+// const btnSubmit = document.querySelector('button[type="submit"]');
+const inputForm = document.querySelector('input[name="email"]');
+const messageForm = document.querySelector('textarea[name="message"]');
+const saveData = localStorage.getItem(DATE_FORM);
+let serializedState;
+
+formFeedback.addEventListener('input', throttle(onSaveData, 500));
+formFeedback.addEventListener('submit', onCleanForm);
+
+if (saveData) {
+    // inputForm.value = JSON.parse(saveData).email;
+    // messageForm.value = JSON.parse(saveData).message;
+    inputForm.value = saveData.email;
+    messageForm.value = saveData.message;
+} else {
+    inputForm.value = '';
+    messageForm.value = '';
+}
+
+function onSaveData(event) {
+    const { name, value } = event.target;
+    dataInputs[name] = value;
+    const { email, message } = dataInputs;
+    if (!email || !message) return;
+     try {
+        serializedState = JSON.stringify(dataInputs);
+        localStorage.setItem(DATE_FORM, serializedState);
+    } catch (error) {
+        console.error("Set state error: ", error.message);
+    }
+}
+
+function onCleanForm(e) {
+    e.preventDefault();
+    console.log(dataInputs);
+    try {
+        localStorage.removeItem(DATE_FORM);
+    } catch (error) {
+        console.error("Set state error: ", error.message);
+    }
+    inputForm.value = '';
+    messageForm.value = '';
+}
