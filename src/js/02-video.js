6191b30b9f49a2293bce80837d58dfd96ab23d22
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-10 21:40:04 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,30 +4,45 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const PLAY_TIME_KEY = "videoplayer-current-time";
 let playTime = 0;
-let serializedState;
+// let serializedState;
 
 player.on('timeupdate', throttle(onTimeUpDate, 1000));
 
-try {
-    serializedState = localStorage.getItem(PLAY_TIME_KEY);
-    if (serializedState !== null) {
-        playTime = JSON.parse(serializedState);
-        player.setCurrentTime(playTime).catch(function (error) {
-            console.error("Set current time error:", error.message);
-        })
-    }
+// try {
+//     serializedState = localStorage.getItem(PLAY_TIME_KEY);
+//     if (serializedState !== null) {
+//         playTime = JSON.parse(serializedState);
+//         player.setCurrentTime(playTime).catch(function (error) {
+//             console.error("Set current time error:", error.message);
+//         })
+//     }
 
-} catch (error) {
-    console.error("Set state error: ", error.message);
-}
+// } catch (error) {
+//     console.error("Set state error: ", error.message);
+// }
+
+// function onTimeUpDate(data) {
+//     playTime = data.seconds;
+//     try {
+//         serializedState = JSON.stringify(playTime);
+//         localStorage.setItem(PLAY_TIME_KEY, serializedState);
+//   } catch (error) {
+//     console.error("Set state error: ", error.message);
+//   }
+// }
 
 function onTimeUpDate(data) {
     playTime = data.seconds;
-    // console.log(playTime);
     try {
-        serializedState = JSON.stringify(playTime);
-        localStorage.setItem(PLAY_TIME_KEY, serializedState);
+        localStorage.setItem(PLAY_TIME_KEY, JSON.stringify(playTime));
   } catch (error) {
     console.error("Set state error: ", error.message);
   }
+}
+
+if (localStorage.getItem(PLAY_TIME_KEY) !== null) {
+    playTime = JSON.parse(localStorage.getItem(PLAY_TIME_KEY));
+        player.setCurrentTime(playTime).catch(function (error) {
+            console.error("Set current time error:", error.message);
+        })
 }
